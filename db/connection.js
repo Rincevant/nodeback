310ccc5b1057e46ada8ab4f1ffcd4eab272e6042
Rincevant <perona.julien@yahoo.fr>
2019-11-06 21:04:23 +0100

    changed deploy 2.0

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,11 @@ const {Client} = require('pg')
 
 // Connection en local
 const client = new Client({
-    user : "user",
-    password : "pass",
-    database: "db",
-    port : 35432
+    user : "bojumrelclrwya",
+    host : "ec2-46-51-190-87.eu-west-1.compute.amazonaws.com",
+    password : "711232073c16b92ec69f06bfe66ff7b361b32a7e4b1be303d26ac8cbef146602",
+    database: "d9kleibpcf1nds",
+    port : 5432
 })
 
 //Connection via docker
