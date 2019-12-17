7dd9d2471fd62044ed78ca088d36ef427dfda704
Rincevant <40612019+Rincevant@users.noreply.github.com>
2019-12-17 14:15:50 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const {Client} = require('pg')
 
 // Connection en local
 const client = new Client({
-    user : "bojumrelclrwya",
-    host : "ec2-46-51-190-87.eu-west-1.compute.amazonaws.com",
-    password : "711232073c16b92ec69f06bfe66ff7b361b32a7e4b1be303d26ac8cbef146602",
-    database: "d9kleibpcf1nds",
+    user : "userDB",
+    host : "hostDB",
+    password : "passDB",
+    database: "databaseName",
     port : 5432
 })
 
